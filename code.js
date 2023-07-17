a6ee07b55940d034b4a6c3f17251d2b0533ee00b
Noah <74479057+reinernoah@users.noreply.github.com>
2023-07-17 16:01:22 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,37 +1,39 @@
-var Count = 0
-setTimeout(function(){
+var Count = 0;
+
+setTimeout(function() {
   document.getElementById("loading").play();
 }, 4200);
 
-setTimeout(function(){
+setTimeout(function() {
   var image = document.getElementById("black");
   image.src = "Glitch.gif";
 }, 4100);
 
-//disable my mute
-function disbleMute() {
-	var video = document.getElementById('content');
-
-              video.muted = false;
-          }
-
+// Disable mute
+function disableMute() {
+  var video = document.getElementById('content');
+  video.muted = false;
+}
 
 function playVid() {
-	var vid = document.getElementById('control');
-    vid.play();
-    }
+  var vid = document.getElementById('control');
+  vid.play();
+}
+
 function stopVid() {
-    var media = document.getElementById('loading');
-    media.pause();
-    media.currentTime = -750;
+  var media = document.getElementById('loading');
+  media.pause();
+  media.currentTime = -750;
 }
+
 function email() {
-    var video = document.getElementById('gmail');
-    video.play();
+  var video = document.getElementById('gmail');
+  video.play();
 }
+
 function resume() {
-    var video = document.getElementById('resume');
-    video.play();
+  var video = document.getElementById('resume');
+  video.play();
 }
 
 // Function to play the loading video
@@ -53,65 +55,56 @@ function adjustVideoSize() {
   video.style.width = videoWidth + 'px';
 }
 
-
-
 window.addEventListener('resize', adjustVideoSize);
 
-
-// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
-// allows the click of a button to prompt the user to create a new grid
+// Create a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
+// Allow the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
-    $("#button1").click(function() {
-        stopVid();
-    	playVid();
-    	var video = document.getElementById('content');
-    	setTimeout(function(){
-    		if (Count == 0){
-    		video.src = "Stranger.mp4";
-    		Count = Count + 1;
-    		}
-    		else if (Count == 1){
-    		video.src = "Banjo.mp4";
-    		Count = Count + 1;
-            email();
-            resume();
-    		}
-    		else if (Count == 2){
-    		video.src = "Jerry.mp4";
-    		Count = Count + 1;
-    		}
-    		else if (Count == 3){
-    		video.src = "OJ.mp4";
-    		Count = 0;
-    		}
-    	}, 750);
-    	disbleMute();
-    });
-    $("#button2").click(function() {
-        stopVid();
-        playVid();
-    	var video = document.getElementById('content');
-    	setTimeout(function(){
-    		if (Count == 0){
-    		video.src = "strange.mp4";
-    		Count = Count + 1;
-    		}
-    		else if (Count == 1){
-    		video.src = "Kla.mp4";
-    		Count = Count + 1;
-            email();
-            resume();
-    		}
-    		else if (Count == 2){
-    		video.src = "Sergio.mp4";
-    		Count = Count + 1;
-    		}
-    		else if (Count == 3){
-    		video.src = "OJ.mp4";
-    		Count = 0;
-    		}
-    	}, 750);
-    	disbleMute()
-	});
-});
+  $("#button1").click(function() {
+    stopVid();
+    playVid();
+    var video = document.getElementById('content');
+    setTimeout(function() {
+      if (Count == 0) {
+        video.src = "Stranger.mp4";
+        Count = Count + 1;
+      } else if (Count == 1) {
+        video.src = "Banjo.mp4";
+        Count = Count + 1;
+        email();
+        resume();
+      } else if (Count == 2) {
+        video.src = "Jerry.mp4";
+        Count = Count + 1;
+      } else if (Count == 3) {
+        video.src = "OJ.mp4";
+        Count = 0;
+      }
+    }, 750);
+    disableMute();
+  });
 
+  $("#button2").click(function() {
+    stopVid();
+    playVid();
+    var video = document.getElementById('content');
+    setTimeout(function() {
+      if (Count == 0) {
+        video.src = "strange.mp4";
+        Count = Count + 1;
+      } else if (Count == 1) {
+        video.src = "Kla.mp4";
+        Count = Count + 1;
+        email();
+        resume();
+      } else if (Count == 2) {
+        video.src = "Sergio.mp4";
+        Count = Count + 1;
+      } else if (Count == 3) {
+        video.src = "OJ.mp4";
+        Count = 0;
+      }
+    }, 750);
+    disableMute();
+  });
+});
