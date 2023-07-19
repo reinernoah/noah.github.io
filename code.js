4869a2bea816c3e085f509597ecef141aef36c87
Noah <74479057+reinernoah@users.noreply.github.com>
2023-07-18 20:40:07 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,33 +1,52 @@
 var Count = 0;
+
 setTimeout(function() {
-  document.getElementById("loading").play();
-}, 4200);
+  var isMobile = isMobileDevice();
+  if (isMobile) {
+    var video = document.getElementById('mobile-div');
+    video.play();
+  } else {
+    document.getElementById("loading").play();
+  }
+}, 3900);
+
 setTimeout(function() {
   var image = document.getElementById("black");
   image.src = "Glitch.gif";
 }, 4100);
+
 // Disable mute
 function disableMute() {
   var video = document.getElementById('content');
   video.muted = false;
 }
+
 function playVid() {
   var vid = document.getElementById('control');
   vid.play();
 }
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+  }
+
 function stopVid() {
   var media = document.getElementById('loading');
   media.pause();
   media.currentTime = -750;
 }
+
 function email() {
   var video = document.getElementById('gmail');
   video.play();
 }
+
 function resume() {
   var video = document.getElementById('resume');
   video.play();
 }
+
+  
 // Function to play the loading video
 function playLoadingVideo() {
   setTimeout(function() {
@@ -37,35 +56,22 @@ function playLoadingVideo() {
 }
 
 
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-  }
-
-
 
 // Function to adjust video size on window resize
 function adjustVideoSize() {
   var video = document.getElementById('video');
   var container = document.getElementsByClassName('container')[0];
   var containerWidth = container.offsetWidth;
+
   // Adjust the video width relative to the container width
   var videoWidth = containerWidth * 0.32; // Adjust the percentage as needed
   video.style.width = videoWidth + 'px';
 }
+
 window.addEventListener('resize', adjustVideoSize);
-$(document).ready(function() {
-  $("#button1").click(function() {
-    // Remove the mobile div on Button 2 click
-    if (window.innerWidth <= 767) {
-      $("#mobile-div").remove();
-    }
-$(document).ready(function() {
-  $("#button2").click(function() {
-    // Remove the mobile div on Button 2 click
-    if (window.innerWidth <= 767) {
-      $("#mobile-div").remove();
-    }
-    
+
+
+
 // Create a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
 // Allow the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
@@ -75,11 +81,14 @@ $(document).ready(function() {
     var video = document.getElementById('content');
     var isMobile = isMobileDevice();
     setTimeout(function() {
-      if (isMobile) {
-        var video = document.getElementById('content');
+      if (window.innerWidth <= 767) {
+         $("#mobile-div").remove();
+      } if (isMobile) {
+        video.src = "Loading1.gif";
+        video.play();
         video.src = "Reel.mp4";
         video.play();
-      if (Count == 0) {
+      } if (Count == 0) {
         video.src = "Stranger.mp4";
         Count = Count + 1;
       } else if (Count == 1) {
@@ -97,18 +106,23 @@ $(document).ready(function() {
     }, 750);
     disableMute();
   });
+
   $("#button2").click(function() {
     stopVid();
     playVid();
     var video = document.getElementById('content');
     var isMobile = isMobileDevice();
     setTimeout(function() {
+      if (window.innerWidth <= 767) {
+         $("#mobile-div").remove();
+      }
       if (isMobile) {
-        var video = document.getElementById('content');
+        video.src = "Loading1.gif";
+        video.play();
         video.src = "Reel.mp4";
         video.play();
-      if (Count == 0) {
-        video.src = "strange.mp4";
+      } if (Count == 0) {
+        video.src = "Stranger.mp4";
         Count = Count + 1;
       } else if (Count == 1) {
         video.src = "Kla.mp4";
