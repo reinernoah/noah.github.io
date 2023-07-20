577ea9a4b39607d0183ec294b60525342beaa1d4
Noah <74479057+reinernoah@users.noreply.github.com>
2023-07-20 16:59:53 -0700

    Update code.js

--- code.js
+++ code.js
@@ -44,23 +44,6 @@ function resume() {
   video.play();
 }
 
-
-
-// Function to adjust video size on window resize
-function adjustVideoSize() {
-  var video = document.getElementById('video');
-  var container = document.getElementsByClassName('container')[0];
-  var containerWidth = container.offsetWidth;
-
-  // Adjust the video width relative to the container width
-  var videoWidth = containerWidth * 0.32; // Adjust the percentage as needed
-  video.style.width = videoWidth + 'px';
-}
-
-window.addEventListener('resize', adjustVideoSize);
-
-
-
 // Create a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
 // Allow the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
@@ -103,7 +86,7 @@ $(document).ready(function() {
         resume();
       } else if (Count == 2) {
         video.src = "Sergio.mp4";
-        Count = Count + 1;
+        Count = 0;
       } 
     }, 750);
     disableMute();
